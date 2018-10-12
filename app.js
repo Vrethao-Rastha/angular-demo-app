3732f60d3ac8c579b8f8e8ec945428eadcb36267
BryanCox <bryanc71@yahoo.com>
2018-10-12 14:07:52 -0700

    asdfisms

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const part = require('path');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
